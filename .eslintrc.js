8e7fa62532d1a05afff2abcb758bc6c6276d2e9c
Ryan Elainska <ryan@elainska.com>
2021-08-07 17:44:18 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.json
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "all",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/explicit-member-accessibility": [
+      "off",
+      {
+        accessibility: "explicit",
+      },
+    ],
+  },
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+const port = 3000
+app.get("/", (_, res) => res.send("Hello World"))
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}.`)
+})
