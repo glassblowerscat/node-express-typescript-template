05d6bbc5712ae08832e26046a40a7d58685dcf5a
Ryan Elainska <ryan@elainska.com>
2021-08-07 19:06:32 -0400

    Create basic schema and serve `/graphql` endpoint

--- index.ts
+++ index.ts
@@ -1,11 +1,52 @@
 // eslint-disable-next-line
 require("dotenv").config()
+import { PrismaClient } from "@prisma/client"
 import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import { makeExecutableSchema } from "@graphql-tools/schema"
+
+const prisma = new PrismaClient()
+
+const typeDefs = `
+  type FileNode {
+	    id: String!
+	    name: String!
+      type: FileNodeType!
+      createdAt: String!
+	  }
+
+	  type Query {
+	    allFiles: [FileNode!]!
+	  }
+
+    enum FileNodeType {
+      DIRECTORY
+      FILE
+      FILE_VERSION
+    }
+	`
+
+const resolvers = {
+  Query: {
+    allFiles: () => {
+      return prisma.fileNode.findMany()
+    },
+  },
+}
+
+export const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+})
 
 const app = express()
-const port = 3000
-app.get("/", (_, res) => res.send("Hello World"))
+app.use(
+  "/graphql",
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+)
 
-app.listen(port, () => {
-  console.log(`Application running on port ${port}.`)
-})
+app.listen(4000)
