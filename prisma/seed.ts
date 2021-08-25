7cf7d68bcd6da7c1dbf07100e4b678bbfbabafe4
Ryan Elainska <ryan@elainska.com>
2021-08-25 18:18:45 -0400

    Add some safety to avoid creating multiple 'root' directories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,10 +5,14 @@ import { createFileRecord } from "../file"
 import { generateFileName, generateId } from "../util/generators"
 import { getMimeTypeFromExtension } from "../util/parsers"
 
-const client = new PrismaClient()
-
 export async function seed(): Promise<void> {
-  const directory = await client.directory.create({ data: { name: "root" } })
+  const client = new PrismaClient()
+  const existingRootDirectory = await client.directory.findFirst({
+    where: { name: "root " },
+  })
+  const rootDirectory =
+    existingRootDirectory ??
+    (await client.directory.create({ data: { name: "root" } }))
   const filesDir = await fs.readdir(`${__dirname}/../../seed-files`)
   for (const file of filesDir) {
     if (file === ".DS_Store") {
@@ -28,7 +32,7 @@ export async function seed(): Promise<void> {
     await createFileRecord(client, {
       name,
       key,
-      directoryId: directory.id,
+      directoryId: rootDirectory.id,
       mimeType,
       size,
     })
