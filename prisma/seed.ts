0ca7ee216c006211a51534952bf40962937b22e9
Ryan Elainska <ryan@elainska.com>
2021-08-25 21:47:47 -0400

    Fix bugs in file reading of seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,47 @@
 import { PrismaClient } from "@prisma/client"
 import { promises as fs } from "fs"
-import { readFile, saveFile } from "../bucket/localBucket"
+import { join } from "path"
+import { saveFile } from "../bucket/localBucket"
 import { createFileRecord } from "../file"
 import { generateFileName, generateId } from "../util/generators"
 import { getMimeTypeFromExtension } from "../util/parsers"
 
 export async function seed(): Promise<void> {
+  const seedFilesPath = `${__dirname}/../../seed-files`
   const client = new PrismaClient()
-  const existingRootDirectory = await client.directory.findFirst({
-    where: { name: "root " },
-  })
-  const rootDirectory =
-    existingRootDirectory ??
-    (await client.directory.create({ data: { name: "root" } }))
-  const filesDir = await fs.readdir(`${__dirname}/../../seed-files`)
-  for (const file of filesDir) {
-    if (file === ".DS_Store") {
-      return
+  try {
+    const existingRootDirectory = await client.directory.findFirst({
+      where: { name: "root" },
+    })
+    const rootDirectory =
+      existingRootDirectory ??
+      (await client.directory.create({ data: { name: "root" } }))
+    const filesDir = await fs.readdir(seedFilesPath)
+    for (const file of filesDir) {
+      if (file === ".DS_Store") {
+        continue
+      }
+      const name = generateFileName()
+      const key = await generateId()
+      const mimeType = getMimeTypeFromExtension(file)
+      const buffer = await fs.readFile(join(seedFilesPath, file))
+      const size = buffer.byteLength
+      await saveFile(key, {
+        ContentLength: size,
+        LastModified: new Date(),
+        ContentType: mimeType,
+        Body: buffer,
+      })
+      await createFileRecord(client, {
+        name,
+        key,
+        directoryId: rootDirectory.id,
+        mimeType,
+        size,
+      })
     }
-    const name = generateFileName()
-    const key = await generateId()
-    const mimeType = getMimeTypeFromExtension(file)
-    const buffer = await readFile(file)
-    const size = buffer.byteLength
-    await saveFile(key, {
-      ContentLength: size,
-      LastModified: new Date(),
-      ContentType: mimeType,
-      Body: buffer,
-    })
-    await createFileRecord(client, {
-      name,
-      key,
-      directoryId: rootDirectory.id,
-      mimeType,
-      size,
-    })
+  } catch (error) {
+    console.error(error)
   }
   await client.$disconnect()
 }
