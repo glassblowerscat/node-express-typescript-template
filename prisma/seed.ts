2d4bbf7181dae64e27336463e45a4b96f2e5d090
Ryan Elainska <ryan@elainska.com>
2021-08-30 17:31:57 -0400

    Update seed function to create sub-directories and put files in them

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client"
 import { promises as fs } from "fs"
 import { join } from "path"
 import { saveFile } from "../bucket/localBucket"
+import { createDirectory } from "../directory"
 import { createFileRecord } from "../file"
 import { generateFileName, generateId } from "../util/generators"
 import { getMimeTypeFromExtension } from "../util/parsers"
@@ -16,11 +17,46 @@ export async function seed(): Promise<void> {
     const rootDirectory =
       existingRootDirectory ??
       (await client.directory.create({ data: { name: "root" } }))
+
+    /**
+     * Let's create a couple child directories
+     * and children of those child directories.
+     * This will help us later when we're testing
+     * operations where we modify whole directories
+     * and want to make sure their descendents get
+     * modified as we would expect.
+     *
+     * We'll make sure to copy some of our files
+     * into each of these directories. To do this,
+     * we'll just re-use some of the files, so there
+     * will be multiple copies of some files, but
+     * that's okay.
+     */
+
+    const subDir1 = await createDirectory(
+      client,
+      "Sub-Directory 1",
+      rootDirectory.id
+    )
+    const subDir2 = await createDirectory(
+      client,
+      "Sub-Directory 2",
+      rootDirectory.id
+    )
+    const subSubDir1 = await createDirectory(
+      client,
+      "Sub-Sub-Directory 1",
+      subDir1.id
+    )
+    const subSubDir2 = await createDirectory(
+      client,
+      "Sub-Sub-Directory 2",
+      subDir1.id
+    )
+
     const filesDir = await fs.readdir(seedFilesPath)
-    for (const file of filesDir) {
-      if (file === ".DS_Store") {
-        continue
-      }
+    const files = filesDir.filter((file) => file !== ".DS_Store")
+    for (const [index, file] of files.entries()) {
       const name = generateFileName()
       const key = await generateId()
       const mimeType = getMimeTypeFromExtension(file)
@@ -32,10 +68,23 @@ export async function seed(): Promise<void> {
         ContentType: mimeType,
         Body: buffer,
       })
+
+      // At time of this comment, we have 114 files in our 'seed-files' folder
+      // At least 21 per folder so we get a third page with pages of 10
+      const directoryId =
+        index < 21
+          ? subSubDir2.id
+          : index < 42
+          ? subSubDir1.id
+          : index < 63
+          ? subDir2.id
+          : index < 84
+          ? subDir1.id
+          : rootDirectory.id
       await createFileRecord(client, {
         name,
         key,
-        directoryId: rootDirectory.id,
+        directoryId,
         mimeType,
         size,
       })
